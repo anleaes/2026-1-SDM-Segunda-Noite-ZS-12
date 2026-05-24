ae9dd7d36954291d81ea02eba627b99a828ddcb9
lvsmendes <lvsmendes2@gmail.com>
2026-05-24 17:34:57 -0300

    feat: models criado

--- aplicativo_de_gestao_escolar/apps/persons/models.py
+++ aplicativo_de_gestao_escolar/apps/persons/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Person(models.Model):
+    name = models.CharField(max_length=100)
+    cpf = models.CharField(max_length=14)
+    email = models.EmailField()
