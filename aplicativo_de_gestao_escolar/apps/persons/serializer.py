f4e2be777c65aa26a9e86fedce31ad6b414d582d
lvsmendes <lvsmendes2@gmail.com>
2026-05-24 18:21:51 -0300

    feat: criado o serializer

--- aplicativo_de_gestao_escolar/apps/persons/serializer.py
+++ aplicativo_de_gestao_escolar/apps/persons/serializer.py
@@ -2,3 +2,6 @@ from rest_framework import viewsets
 from .models import Person
 from .serializer import PersonSerializer
 
+class Person(viewsets.ModelViewSet):
+    queryset = Person.objects.all()
+    serializer_class = PersonSerializer
