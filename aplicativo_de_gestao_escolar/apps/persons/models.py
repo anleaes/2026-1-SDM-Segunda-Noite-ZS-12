b3ad62375d6b048e83b07e05f2c3b614ba8996c2
lvsmendes <lvsmendes2@gmail.com>
2026-05-24 17:36:25 -0300

    feat: adicionado classe meta e def str ao models

--- aplicativo_de_gestao_escolar/apps/persons/models.py
+++ aplicativo_de_gestao_escolar/apps/persons/models.py
@@ -5,3 +5,11 @@ class Person(models.Model):
     name = models.CharField(max_length=100)
     cpf = models.CharField(max_length=14)
     email = models.EmailField()
+
+class Meta:
+    verbose_name = 'Pessoa'
+    verbose_name_plural = 'Pessoas'
+    ordering = ['id']
+
+def __str__(self): 
+    return self.name
