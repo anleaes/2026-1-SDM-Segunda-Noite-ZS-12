9c78e01f64eea556c17b6f54d4ea38d86289ba6b
lvsmendes <lvsmendes2@gmail.com>
2026-05-24 18:43:13 -0300

    feat: view criado

--- aplicativo_de_gestao_escolar/apps/persons/views.py
+++ aplicativo_de_gestao_escolar/apps/persons/views.py
@@ -4,3 +4,6 @@ from .models import Person
 from .serializer import PersonSerializer
 
 # Create your views here.
+class PersonViewSet(viewsets.ModelViewSet):
+    queryset = Person.objects.all()
+    serializer_class = PersonSerializer
