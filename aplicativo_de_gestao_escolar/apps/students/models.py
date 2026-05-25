fc315b4435a3ce32b0947d6efb36062d71793c58
lvsmendes <lvsmendes2@gmail.com>
2026-05-25 20:59:00 -0300

    feat: começo da criacao de models

--- aplicativo_de_gestao_escolar/apps/students/models.py
+++ aplicativo_de_gestao_escolar/apps/students/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from persons.models import Person
 
 # Create your models here.
+class Student(Person):
+    matricula = models.CharField('Matricula', max_length=20, unique=True)
+    dataNascimento = models.CharField('Data de nascimento,',max_length=10)
