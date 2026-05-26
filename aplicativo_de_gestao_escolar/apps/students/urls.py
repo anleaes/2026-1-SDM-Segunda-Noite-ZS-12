6fabcbb7da2a58b3bbcc40d2f87e70e987a98c3c
lvsmendes <lvsmendes2@gmail.com>
2026-05-25 21:29:45 -0300

    feat: urls feito

--- aplicativo_de_gestao_escolar/apps/students/urls.py
+++ aplicativo_de_gestao_escolar/apps/students/urls.py
@@ -1,3 +1,12 @@
 from django.urls import path, include
 from . import views
 from rest_framework import routers
+
+app_name = 'students'
+
+router = routers.SimpleRouter()
+router.register('', views.PersonViewSet, basename='pessoas')
+
+urlpatterns = [
+    path('', include(router.urls) )
+]
