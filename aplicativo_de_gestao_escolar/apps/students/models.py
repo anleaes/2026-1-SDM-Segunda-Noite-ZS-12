8ab17ff14a40a5874aad4a62611e31f143064455
lvsmendes <lvsmendes2@gmail.com>
2026-05-25 21:42:37 -0300

    fix: models

--- aplicativo_de_gestao_escolar/apps/students/models.py
+++ aplicativo_de_gestao_escolar/apps/students/models.py
@@ -2,19 +2,24 @@ from django.db import models
 from persons.models import Person
 
 # Create your models here.
+from django.db import models
+from persons.models import Person
+
 class Student(Person):
-    matricula = models.CharField('Matricula', max_length=20, unique=True)
-    dataNascimento = models.CharField('Data de nascimento,',max_length=10)
-    status = [
+    STATUS_CHOICES = [
         ('ativo', 'Ativo'),
         ('inativo', 'Inativo'),
-        ('Trancado', 'Trancado'),
+        ('trancado', 'Trancado'),
     ]
 
-class Meta:
-    verbose_name = 'Aluno'
-    verbose_name_plural = 'Alunos'
-    ordering = ['id']
+    matricula = models.CharField('Matricula', max_length=20, unique=True)
+    data_nascimento = models.DateField('Data de nascimento')
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='ativo')
 
-def __str__(self): 
-    return self.name
+    class Meta:
+        verbose_name = 'Aluno'
+        verbose_name_plural = 'Alunos'
+        ordering = ['id']
+
+    def __str__(self):
+        return self.name
