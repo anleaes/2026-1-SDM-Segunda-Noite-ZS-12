399fae6278318fe22ca72e73cea5668efb938504
lvsmendes <lvsmendes2@gmail.com>
2026-05-26 16:52:33 -0300

    fix: serializer para mostrar o status

--- aplicativo_de_gestao_escolar/apps/students/models.py
+++ aplicativo_de_gestao_escolar/apps/students/models.py
@@ -4,7 +4,7 @@ from persons.models import Person
 # Create your models here.
 class Student(Person):
     matricula = models.CharField('Matricula', max_length=20, unique=True)
-    dataNascimento = models.CharField('Data de nascimento,',max_length=10)
+    data_nascimento = models.CharField('Data de nascimento,',max_length=10)
     status = [
         ('ativo', 'Ativo'),
         ('inativo', 'Inativo'),
