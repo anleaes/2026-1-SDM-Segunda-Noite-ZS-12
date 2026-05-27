d1d81654c9a1240f2c48bfd7f12efe6c21eae3a6
lvsmendes <lvsmendes2@gmail.com>
2026-05-26 21:13:49 -0300

    feat: classe models criada

--- aplicativo_de_gestao_escolar/apps/teachers/models.py
+++ aplicativo_de_gestao_escolar/apps/teachers/models.py
@@ -2,3 +2,13 @@ from django.db import models
 from persons.models import Person
 
 # Create your models here.
+class Teacher(Person):
+    titulacao = models.CharField('titulacao', max_length=20)
+    registro = models.CharField('registro', max_length=20)
+
+    class Meta:
+        verbose_name = 'Professor'
+        verbose_name_plural = 'Professores'
+
+    def __str__(self):
+        return self.name
