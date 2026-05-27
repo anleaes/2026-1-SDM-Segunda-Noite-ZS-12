9a60d20f2e5f519f869663c71214ece90dd1a936
WesleyDev0101 <wesley.coldyto@gmail.com>
2026-05-26 22:33:32 -0300

    Criação do models

--- aplicativo_de_gestao_escolar/apps/classes/models.py
+++ aplicativo_de_gestao_escolar/apps/classes/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Turma(models.Model):
+    codigo = models.CharField('Código', max_length=20)
+    turno = models.CharField('Turno', max_length=20)
+    ano_letivo = models.IntegerField('Ano Letivo')
+
