496d7ead4cbf34b530bac80a86d3714b27a9c294
WesleyDev0101 <wesley.coldyto@gmail.com>
2026-05-26 22:35:05 -0300

    Finalização da models

--- aplicativo_de_gestao_escolar/apps/classes/models.py
+++ aplicativo_de_gestao_escolar/apps/classes/models.py
@@ -6,3 +6,17 @@ class Turma(models.Model):
     turno = models.CharField('Turno', max_length=20)
     ano_letivo = models.IntegerField('Ano Letivo')
 
+    professor = models.ForeignKey(
+        'Professor',
+        on_delete=models.CASCADE,
+        related_name='Turmas',
+        verbose_name='Professor'
+    )
+
+    class Meta:
+        verbose_name = 'Turma'
+        verbose_name_plural = 'Turmas'
+        ordering = ['id']
+
+    def __str__(self):
+        return f'{self.codigo} - {self.turno} ({self.ano_letivo})'
