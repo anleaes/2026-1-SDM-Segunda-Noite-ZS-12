a59ef95d99890706c0267adb8d717c5593104245
lvsmendes <lvsmendes2@gmail.com>
2026-05-26 21:26:19 -0300

    feat: views concluido

--- aplicativo_de_gestao_escolar/apps/teachers/views.py
+++ aplicativo_de_gestao_escolar/apps/teachers/views.py
@@ -3,3 +3,7 @@ from rest_framework import viewsets
 from .models import Teacher
 from .serializer import TeacherSerializer
 # Create your views here.
+
+class TeacherViewSet(viewsets.ModelViewSet):
+    queryset = Teacher.objects.all()
+    serializer_class = TeacherSerializer
