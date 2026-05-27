101548eb9b44bd97ae9182f9ff3a4d926acafad4
WesleyDev0101 <wesley.coldyto@gmail.com>
2026-05-26 22:45:27 -0300

    Criação da View

--- aplicativo_de_gestao_escolar/apps/classes/views.py
+++ aplicativo_de_gestao_escolar/apps/classes/views.py
@@ -3,3 +3,7 @@ from rest_framework import viewsets
 from .models import Class
 from .serializer import ClassSerializer
 
+# Create your views here.
+class ClassViewSet(viewsets.ModelViewSet):
+    queryset = Class.objects.all()
+    serializer_class = ClassSerializer  
