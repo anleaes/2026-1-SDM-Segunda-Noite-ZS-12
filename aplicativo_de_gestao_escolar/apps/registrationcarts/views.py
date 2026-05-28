a2a91cc868081a80fefcd08c98b58848cb8ba25a
WesleyDev0101 <wesley.coldyto@gmail.com>
2026-05-28 18:12:22 -0300

    Construção da Views

--- aplicativo_de_gestao_escolar/apps/registrationcarts/views.py
+++ aplicativo_de_gestao_escolar/apps/registrationcarts/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import RegistrationCart
+from .serializer import RegistrationCartSerializer
 
 # Create your views here.
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = RegistrationCart.objects.all()
+    serializer_class = RegistrationCartSerializer  
