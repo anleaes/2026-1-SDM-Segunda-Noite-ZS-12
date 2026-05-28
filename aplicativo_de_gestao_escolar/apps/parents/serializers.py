46d96bb30948c9803ccee2ed1e22c88a3050771c
lvsmendes <lvsmendes2@gmail.com>
2026-05-28 18:08:52 -0300

    feat: serializer criado

--- aplicativo_de_gestao_escolar/apps/parents/serializers.py
+++ aplicativo_de_gestao_escolar/apps/parents/serializers.py
@@ -1,3 +1,7 @@
 from .models import Parent
 from rest_framework import serializers
 
+class ParentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Parent
+        fields = '__all__'
