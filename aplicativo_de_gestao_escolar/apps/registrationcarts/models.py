a32fbd0ddd26db2e4a97d636c604593489861623
WesleyDev0101 <wesley.coldyto@gmail.com>
2026-05-28 18:04:01 -0300

    Criação do Models

--- aplicativo_de_gestao_escolar/apps/registrationcarts/models.py
+++ aplicativo_de_gestao_escolar/apps/registrationcarts/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+class RegistrationCart(models.Model):
+    status = models.CharField('Status', max_length=30)
+
+    registration = models.ForeignKey(
+        'registrations.Registration',
+        on_delete=models.CASCADE,
+        related_name='registration_carts',
+        verbose_name='Matrícula'
+    )
+
+    discipline = models.ForeignKey(
+        'disciplines.Discipline',
+        on_delete=models.CASCADE,
+        related_name='registration_carts',
+        verbose_name='Disciplina'
+    )
+
+    teacher = models.ForeignKey(
+        'teachers.Teacher',
+        on_delete=models.CASCADE,
+        related_name='registration_carts',
+        verbose_name='Professor'
+    )
+
