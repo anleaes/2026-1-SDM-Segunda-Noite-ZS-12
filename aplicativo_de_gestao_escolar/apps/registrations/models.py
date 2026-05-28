38db73cd71e2db915791541c82e214cee5f0c7f2
WesleyDev0101 <wesley.coldyto@gmail.com>
2026-05-28 18:41:21 -0300

    Criação do Models

--- aplicativo_de_gestao_escolar/apps/registrations/models.py
+++ aplicativo_de_gestao_escolar/apps/registrations/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Registration(models.Model):
+    data_matricula = models.DateField('Data da Matrícula')
+    situacao = models.CharField('Situação', max_length=30)
+
+    student = models.ForeignKey(
+        'students.Student',
+        on_delete=models.CASCADE,
+        related_name='registrations',
+        verbose_name='Aluno'
+    )
+
+    school_class = models.ForeignKey(
+        'classes.Class',
+        on_delete=models.CASCADE,
+        related_name='registrations',
+        verbose_name='Turma'
+    )
+
