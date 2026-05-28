c4eb441fedd1f4fa987664a9d8886d7405f2457f
WesleyDev0101 <wesley.coldyto@gmail.com>
2026-05-28 19:16:05 -0300

    Views Concluída

--- aplicativo_de_gestao_escolar/apps/classrooms/views.py
+++ aplicativo_de_gestao_escolar/apps/classrooms/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from rest_framework import viewsets
-from .models import Category
-from .serializer import CategorySerializer
+from .models import Classroom
+from .serializer import ClassroomSerializer
 
 
 # Create your views here.
+class ClassroomViewSet(viewsets.ModelViewSet):
+    queryset = Classroom.objects.all()
+    serializer_class = ClassroomSerializer  
