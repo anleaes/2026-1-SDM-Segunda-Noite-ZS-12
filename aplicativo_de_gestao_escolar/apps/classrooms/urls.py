2788f6f268e1b93609cfcedb1b16b3193610bef9
WesleyDev0101 <wesley.coldyto@gmail.com>
2026-05-28 19:17:47 -0300

    Urls Conluído

--- aplicativo_de_gestao_escolar/apps/classrooms/urls.py
+++ aplicativo_de_gestao_escolar/apps/classrooms/urls.py
@@ -1,3 +1,12 @@
 from django.urls import path, include
 from . import views
 from rest_framework import routers
+
+app_name = 'classrooms'
+
+router = routers.SimpleRouter()
+router.register('', views.ClassroomViewSet, basename='classrooms')
+
+urlpatterns = [
+    path('', include(router.urls) )
+]
