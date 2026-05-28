144efd62054ed32aba3bc76e25617c4604b9a042
lvsmendes <lvsmendes2@gmail.com>
2026-05-28 18:25:25 -0300

    feat: urls de parents feito

--- aplicativo_de_gestao_escolar/apps/parents/urls.py
+++ aplicativo_de_gestao_escolar/apps/parents/urls.py
@@ -1,3 +1,12 @@
 from django.urls import path, include
 from . import views
 from rest_framework import routers
+
+app_name = 'parents'
+
+router = routers.SimpleRouter()
+router.register('', views.ParentViewSet, basename='responsaveis')
+
+urlpatterns = [
+    path('', include(router.urls))
+]
