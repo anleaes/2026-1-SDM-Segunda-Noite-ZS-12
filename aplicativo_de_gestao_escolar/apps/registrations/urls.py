b369f231a2e42f2e3f525e78cce1252915692907
WesleyDev0101 <wesley.coldyto@gmail.com>
2026-05-28 18:57:29 -0300

    Urls Concluído

--- aplicativo_de_gestao_escolar/apps/registrations/urls.py
+++ aplicativo_de_gestao_escolar/apps/registrations/urls.py
@@ -1,3 +1,12 @@
 from django.urls import path, include
 from . import views
 from rest_framework import routers
+
+app_name = 'registrations'
+
+router = routers.SimpleRouter()
+router.register('', views.RegistrationViewSet, basename='registrations')
+
+urlpatterns = [
+    path('', include(router.urls) )
+]
