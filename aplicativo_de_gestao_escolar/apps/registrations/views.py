5debbdb3c75dcc93df81b3c5f8c09f3f562c9468
WesleyDev0101 <wesley.coldyto@gmail.com>
2026-05-28 18:51:17 -0300

    Importações para a Views

--- aplicativo_de_gestao_escolar/apps/registrations/views.py
+++ aplicativo_de_gestao_escolar/apps/registrations/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Registration
+from .serializer import RegistrationSerializer
 
 # Create your views here.
