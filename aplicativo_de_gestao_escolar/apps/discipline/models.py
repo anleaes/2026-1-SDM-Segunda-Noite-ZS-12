cd29da484627c8a1a2417591103cf65b89ccd880
Carlos Eduardo Soter Muller <carlosmullerr1@gmail.com>
2026-05-28 17:34:49 -0300

    exlcuido o models da branch

--- aplicativo_de_gestao_escolar/apps/discipline/models.py
+++ aplicativo_de_gestao_escolar/apps/discipline/models.py
@@ -1,6 +1,2 @@
 from django.db import models
 
-class Discipline(models.Model):
-    name = models.CharField('Name', max_length=100)
-    workload = models.IntegerField('Workload')
-    code = models.CharField('Code', max_length=20)
