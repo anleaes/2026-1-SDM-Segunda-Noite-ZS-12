759e5c0c92f7939d8c5e4d9d50592960aa57ea02
WesleyDev0101 <wesley.coldyto@gmail.com>
2026-05-28 19:08:14 -0300

    Criação do Models

--- aplicativo_de_gestao_escolar/apps/classrooms/models.py
+++ aplicativo_de_gestao_escolar/apps/classrooms/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Classroom(models.Model):
+    data = models.DateField('Data')
+    sala = models.CharField('Sala', max_length=30)
+    conteudo = models.TextField('Conteúdo', max_length=255)
+
+    # Relacionamentos
+    registration = models.ForeignKey(
+        'registrations.Registration',
+        on_delete=models.CASCADE,
+        related_name='classrooms',
+        verbose_name='Matrícula'
+    )
+
+    discipline = models.ForeignKey(
+        'disciplines.Discipline',
+        on_delete=models.CASCADE,
+        related_name='classrooms',
+        verbose_name='Disciplina'
+    )
+
