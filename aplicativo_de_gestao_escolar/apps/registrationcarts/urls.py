0cab0d7a2ee171f93ac74ea5318d6fc9a7ea044a
WesleyDev0101 <wesley.coldyto@gmail.com>
2026-05-28 18:16:41 -0300

    Finalização do Urls

--- aplicativo_de_gestao_escolar/apps/registrationcarts/urls.py
+++ aplicativo_de_gestao_escolar/apps/registrationcarts/urls.py
@@ -1,3 +1,12 @@
 from django.urls import path, include
 from . import views
 from rest_framework import routers
+
+app_name = 'registrationcarts'
+
+router = routers.SimpleRouter()
+router.register('', views.RegistrationCartViewSet, basename='registrationcart')
+
+urlpatterns = [
+    path('', include(router.urls) )
+]
