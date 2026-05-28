cf8b1906cdbd6bc956f2602438a5d6c7d7422037
Carlos Eduardo Soter Muller <carlosmullerr1@gmail.com>
2026-05-28 17:33:07 -0300

    craiado a classe discipline

--- aplicativo_de_gestao_escolar/apps/discipline/models.py
+++ aplicativo_de_gestao_escolar/apps/discipline/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Discipline(models.Model):
+    name = models.CharField('Name', max_length=100)
+    workload = models.IntegerField('Workload')
+    code = models.CharField('Code', max_length=20)
