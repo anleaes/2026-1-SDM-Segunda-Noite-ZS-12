5f25c84dc681c4d17b078d6597513aa32cfd888e
Cadumuller <carlosmullerr1@gmail.com>
2026-05-29 19:33:32 -0300

    finalizado arquivo urls discilpline

--- aplicativo_de_gestao_escolar/apps/discipline/urls.py
+++ aplicativo_de_gestao_escolar/apps/discipline/urls.py
@@ -1,3 +1,12 @@
 from django.urls import path, include
 from . import views
 from rest_framework import routers
+
+app_name = 'discipline'
+
+router = routers.SimpleRouter()
+router.register('', views.DisciplineViewSet, basename='disciplina')
+
+urlpatterns = [
+    path('', include(router.urls) )
+]
