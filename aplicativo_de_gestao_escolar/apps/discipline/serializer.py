13214417313df8b36b35e23edb40b6322ff4b47b
Cadumuller <carlosmullerr1@gmail.com>
2026-05-29 19:25:59 -0300

    criado a class dentro do seriealizer de discilpine

--- aplicativo_de_gestao_escolar/apps/discipline/serializer.py
+++ aplicativo_de_gestao_escolar/apps/discipline/serializer.py
@@ -2,3 +2,7 @@ from .models import Discipline
 from rest_framework import serializers
 
 
+class DisciplineSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Discipline
+        fields = '__all__'
