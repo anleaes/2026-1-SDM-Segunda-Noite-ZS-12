5a7c1cef19864333b62e067b9afe9acd6613a60a
Cadumuller <carlosmullerr1@gmail.com>
2026-05-29 19:28:45 -0300

    criado class views dentro de discipline

--- aplicativo_de_gestao_escolar/apps/discipline/views.py
+++ aplicativo_de_gestao_escolar/apps/discipline/views.py
@@ -4,3 +4,8 @@ from rest_framework import viewsets
 from .models import Category
 from .serializer import CategorySerializer
 
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer  
+
+
