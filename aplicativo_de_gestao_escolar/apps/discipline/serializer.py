f19e57c3de0b7a13580602fb0d514c8561bee952
Cadumuller <carlosmullerr1@gmail.com>
2026-05-29 20:00:40 -0300

    feita alteracoes necessarias

--- aplicativo_de_gestao_escolar/apps/discipline/serializer.py
+++
@@ -1,8 +0,0 @@
-from .models import Discipline
-from rest_framework import serializers
-
-
-class DisciplineSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Discipline
-        fields = '__all__'
