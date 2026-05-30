7443b5e69d50604fdec9a474ad1f2f9457fb1002
Carlos Eduardo Soter Muller <carlosmullerr1@gmail.com>
2026-05-30 00:22:37 -0300

    craido class meta no arq models

--- aplicativo_de_gestao_escolar/apps/bulletinitem/models.py
+++ aplicativo_de_gestao_escolar/apps/bulletinitem/models.py
@@ -19,3 +19,10 @@ class Bulletinitem(models.Model):
         verbose_name='Disciplines'
     )
 
+    class Meta:
+        verbose_name = 'Bulletin Item'
+        verbose_name_plural = 'Bulletin Items'
+        ordering = ['id']
+
+    def __str__(self):
+        return f'{self.discipline} - {self.situation}'
