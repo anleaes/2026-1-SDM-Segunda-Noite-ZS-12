623e35e3fe5da577e69664dc55638cd129d17b42
Carlos Eduardo Soter Muller <carlosmullerr1@gmail.com>
2026-05-29 23:18:48 -0300

    criado models bulletins

--- aplicativo_de_gestao_escolar/apps/bulletins/models.py
+++ aplicativo_de_gestao_escolar/apps/bulletins/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Bulletins(models.Model):
+    school_year = models.IntegerField('School Year')
+    semester = models.IntegerField('Semester')
+    final_situation = models.CharField('Final Situation', max_length=50)
+
+    student = models.OneToOneField(
+        'students.Student',
+        on_delete=models.CASCADE,
+        related_name='bulletin',
+        verbose_name='Student'
+    )
