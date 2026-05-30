e6fadb7dbb642bac0944c90f5c1f851feaeaabb4
Carlos Eduardo Soter Muller <carlosmullerr1@gmail.com>
2026-05-29 23:23:03 -0300

    criado o class dentro do arquivo serializer

--- aplicativo_de_gestao_escolar/apps/bulletins/serializer.py
+++ aplicativo_de_gestao_escolar/apps/bulletins/serializer.py
@@ -1,3 +1,7 @@
 from .models import Bulletins
 from rest_framework import serializers
 
+class BulletinsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Bulletins
+        fields = '__all__'
