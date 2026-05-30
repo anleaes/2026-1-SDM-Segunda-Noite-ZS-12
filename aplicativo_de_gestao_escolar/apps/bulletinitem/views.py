affeee0cc68174da73daa2763b8a6f7106ab09d6
Carlos Eduardo Soter Muller <carlosmullerr1@gmail.com>
2026-05-30 00:27:20 -0300

    criado impor do views

--- aplicativo_de_gestao_escolar/apps/bulletinitem/views.py
+++ aplicativo_de_gestao_escolar/apps/bulletinitem/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Bulletinitem
+from .serializer import BulletinitemSerializer
 
-# Create your views here.
+
