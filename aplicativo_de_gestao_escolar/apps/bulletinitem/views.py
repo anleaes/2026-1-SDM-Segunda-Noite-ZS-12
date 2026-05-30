4fb73c4ef033a0cf3475ef46bb01e26806c5c3b6
Carlos Eduardo Soter Muller <carlosmullerr1@gmail.com>
2026-05-30 00:27:36 -0300

    criado o class no arq views

--- aplicativo_de_gestao_escolar/apps/bulletinitem/views.py
+++ aplicativo_de_gestao_escolar/apps/bulletinitem/views.py
@@ -4,3 +4,7 @@ from .models import Bulletinitem
 from .serializer import BulletinitemSerializer
 
 
+
+class BulletinitemViewSet(viewsets.ModelViewSet):
+    queryset = Bulletinitem.objects.all()
+    serializer_class = BulletinitemSerializer  
