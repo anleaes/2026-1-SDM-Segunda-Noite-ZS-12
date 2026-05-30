1ff9898d63c4dd3e28bee417419fdd83abbe8735
Carlos Eduardo Soter Muller <carlosmullerr1@gmail.com>
2026-05-30 00:29:39 -0300

    criado o router em urls

--- aplicativo_de_gestao_escolar/apps/bulletinitem/urls.py
+++ aplicativo_de_gestao_escolar/apps/bulletinitem/urls.py
@@ -1,3 +1,12 @@
 from django.urls import path, include
 from . import views
 from rest_framework import routers
+
+app_name = 'bulletinitems'
+
+router = routers.SimpleRouter()
+router.register('', views.CategoryViewSet, basename='itemboletim')
+
+urlpatterns = [
+    path('', include(router.urls) )
+]
