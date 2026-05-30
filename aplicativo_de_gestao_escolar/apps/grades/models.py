5b2da273eb8e1639c844eaded1c1278c2eb061ec
Carlos Eduardo Soter Muller <carlosmullerr1@gmail.com>
2026-05-29 23:44:02 -0300

    criado class grades no models e feito a correcao do settings

--- aplicativo_de_gestao_escolar/apps/grades/models.py
+++ aplicativo_de_gestao_escolar/apps/grades/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Grades(models.Model):
+    bimester = models.IntegerField('Bimester')
+    value = models.FloatField('Value')
+    type = models.CharField('Type', max_length=50)
+
+    bulletin = models.ForeignKey(
+        'bulletins.Bulletins',
+        on_delete=models.CASCADE,
+        related_name='grades',
+        verbose_name='Bulletins'
+    )
