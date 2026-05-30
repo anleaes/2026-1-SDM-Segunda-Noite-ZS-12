be219cf1f36da750b9fdcedaa7b949c0afa887b6
Carlos Eduardo Soter Muller <carlosmullerr1@gmail.com>
2026-05-29 23:26:36 -0300

    criado o class dentro de views

--- aplicativo_de_gestao_escolar/apps/bulletins/views.py
+++ aplicativo_de_gestao_escolar/apps/bulletins/views.py
@@ -4,3 +4,6 @@ from .models import Bulletins
 from .serializer import BulletinsSerializer
 
 # Create your views here.
+class BulletinsViewSet(viewsets.ModelViewSet):
+    queryset = Bulletins.objects.all()
+    serializer_class = BulletinsSerializer  
